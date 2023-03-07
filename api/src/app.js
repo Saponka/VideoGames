22d2715755adae94eba730c989f07b8f7eac958a
Franco <francoherrera_8@hotmail.com.ar>
2023-03-07 19:29:48 -0300

    cors2

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+const cors = require('cors')
 require('./db.js');
 
 const server = express();
@@ -14,9 +14,12 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+//cors 
+server.use(cors());
+
 //middleware de headers
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); //'*' update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); //'*' update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -24,6 +27,8 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
+
+
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
