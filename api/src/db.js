60437d8e450364917daf458baee8e817e1495077
Franco <francoherrera_8@hotmail.com.ar>
2023-03-07 19:23:57 -0300

    .env

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
  /*  DB_USER, DB_PASSWORD, DB_HOST, */DB_DEPLOY
 } = process.env;
 
- var sequelize = new Sequelize(`${DB_DEPLOY}/videogames`, {
+ var sequelize = new Sequelize(`${DB_DEPLOY}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); 
