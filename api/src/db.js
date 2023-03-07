c798dd2646ed0f7db8ddfc95cbd50a1cb8272899
Franco <francoherrera_8@hotmail.com.ar>
2023-03-07 20:13:57 -0300

    cors222

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
- /*  DB_USER, DB_PASSWORD, DB_HOST, */DB_DEPLOY
+ /*  DB_USER, DB_PASSWORD, DB_HOST, */ DB_DEPLOY
 } = process.env;
 
- var sequelize = new Sequelize(`${DB_DEPLOY}`, {
+ var sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); 
