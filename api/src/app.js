6be115357e51753a09d51ff11183884f810fb019
Franco <francoherrera_8@hotmail.com.ar>
2023-03-08 18:35:56 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -7,8 +7,9 @@ const cors = require('cors');
 require('./db.js');
 
 const server = express();
-
 server.name = 'API';
+//cors
+server.use(cors());
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
