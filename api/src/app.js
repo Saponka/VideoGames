8cc525f729c3c494392f96e4e57b0c040067be5f
Franco <francoherrera_8@hotmail.com.ar>
2023-03-08 21:23:29 -0300

    cors222

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(morgan('dev'));
 
 //middleware de headers
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogames-production-1c30.up.railway.app'); //'*' update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); //'*' update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
